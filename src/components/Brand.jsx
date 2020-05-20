cebdfb37bf437a2d4add474a2a05a66ab2f61d1e
Cameron Lengerich <clengerich@apterainc.com>
2020-05-20 16:12:30 -0400

    fix: Character limit in mobber name (#22) fix: Increase font-size of title

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import './Brand.scss';
 
 const Brand = () => {
     const history = useHistory();
--- src/components/Mobber.jsx
+++ src/components/Mobber.jsx
@@ -1,27 +1,27 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useMobbers } from "../context/MobbersContext";
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useMobbers } from '../context/MobbersContext';
 
 const Mobber = ({ mobber }) => {
     const { removeMobber } = useMobbers();
 
     const isDriver = () => {
-        return mobber.role === "driver";
+        return mobber.role === 'driver';
     };
 
     const isNavigator = () => {
-        return mobber.role === "navigator";
+        return mobber.role === 'navigator';
     };
 
     return (
         <div className={`mobber ${mobber.role}`}>
-            {isDriver() && <FontAwesomeIcon icon="car" className="role" />}
-            {isNavigator() && <FontAwesomeIcon icon="map-signs" className="role" />}
-            {!isDriver() && !isNavigator() && <div className="role"></div>}
-            {mobber.name}
+            {isDriver() && <FontAwesomeIcon icon='car' className='role' />}
+            {isNavigator() && <FontAwesomeIcon icon='map-signs' className='role' />}
+            {!isDriver() && !isNavigator() && <div className='role'></div>}
+            <div className='name'>{mobber.name}</div>
             <FontAwesomeIcon
-                icon="times"
-                className="close"
+                icon='times'
+                className='close'
                 onClick={() => removeMobber(mobber)}
             />
         </div>
