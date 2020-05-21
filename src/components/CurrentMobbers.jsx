190496c1abfa23c52d55578e134cd16b2a978e31
Cameron Lengerich <clengerich@apterainc.com>
2020-05-21 12:05:39 -0400

    fix: Limit characters in active driver/navigation name (#34). fix: Mobile sizing of very long name fix: Clipboard location

--- src/components/CurrentMobbers.jsx
+++ src/components/CurrentMobbers.jsx
@@ -1,23 +1,23 @@
-import React from "react";
-import { useMobbers } from "../context/MobbersContext";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "./CurrentMobbers.scss";
+import React from 'react';
+import { useMobbers } from '../context/MobbersContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './CurrentMobbers.scss';
 
 const CurrentMobbers = () => {
     const { driver, navigator } = useMobbers();
 
     return (
-        <div className="current-mobbers">
+        <div className='current-mobbers'>
             {driver && (
-                <div className="current-role">
-                    <FontAwesomeIcon icon="car" className="role" />
-                    {driver.name}
+                <div className='current-role'>
+                    <FontAwesomeIcon icon='car' className='role' />
+                    <div className='name'>{driver.name}</div>
                 </div>
             )}
             {navigator && (
-                <div className="current-role">
-                    <FontAwesomeIcon icon="map-signs" className="role" />
-                    {navigator.name}
+                <div className='current-role'>
+                    <FontAwesomeIcon icon='map-signs' className='role' />
+                    <div className='name'>{navigator.name}</div>
                 </div>
             )}
         </div>
