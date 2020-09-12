125da0f77e064ca60b1fccca9a5df5790e01829b
Cameron Lengerich <clengerich@apterainc.com>
2020-09-11 22:17:56 -0400

    feat: Play notification sound when selected #73

--- src/components/AudioSelection.jsx
+++ src/components/AudioSelection.jsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Popover from './shared/Popover';
+import Audio from './shared/Audio';
 import { useAudio } from '../context/AudioContext';
-import './AudioSelection.scss';
 import { strings } from '../strings';
+import './AudioSelection.scss';
 
 const AudioSelection = ({ position = 'bottom' }) => {
     const { audioFile, options, setAudioFile } = useAudio();
+    const [previewAudioFile, setPreviewAudioFile] = useState(false);
+
+    useEffect(() => {
+        if (previewAudioFile) {
+            setTimeout(() => {
+                setPreviewAudioFile(false);
+            }, 1000);
+        }
+    }, [previewAudioFile]);
+
+    const handleClick = (option) => {
+        setAudioFile(option.file);
+        setPreviewAudioFile(true);
+    };
 
     const jsx = (
         <div className='popover audio-selection-container'>
             {options.map((option) => (
                 <div
                     className={`audio-option ${option.selected ? 'selected' : ''}`}
-                    onClick={() => setAudioFile(option.file)}
+                    onClick={() => handleClick(option)}
                     key={option.file}>
                     {option.name}
                 </div>
@@ -28,14 +43,20 @@ const AudioSelection = ({ position = 'bottom' }) => {
     };
 
     return (
-        <Popover
-            jsx={jsx}
-            className='audio-selection'
-            position={position}
-            render={() => (
-                <FontAwesomeIcon icon={getIconName()} title='Select notification sound' />
-            )}
-        />
+        <>
+            <Popover
+                jsx={jsx}
+                className='audio-selection'
+                position={position}
+                render={() => (
+                    <FontAwesomeIcon
+                        icon={getIconName()}
+                        title='Select notification sound'
+                    />
+                )}
+            />
+            {previewAudioFile && <Audio />}
+        </>
     );
 };
 
